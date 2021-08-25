2b521a30631fe0ddd497f5e61b8a3a6b5b186efb
Aline Viana <alinemaciel018@gmail.com>
2021-08-25 10:09:14 -0300

    exercises

---
+++ functionWithParams.js
@@ -0,0 +1,17 @@
+// Crie uma função chamada functionWithArgsque aceite dois argumentos e envie sua soma para o console dev.
+// Chame a função com dois números como argumentos.
+
+function functionWithArgs(num1, num2){
+    return num1 + num2
+  }
+  
+  console.log(functionWithArgs(1,2))
+  console.log(functionWithArgs(7,9))
+
+// Como o exercício pede: 
+// function functionWithArgs(num1, num2){
+//     console.log(num1 + num2)
+//       }
+    
+//     functionWithArgs(1,2)
+//     functionWithArgs(7,9)
---
+++ scapeString.js
@@ -0,0 +1,4 @@
+var myStr = "\t\rFirstLine\n\t\\SecondLine\nThirdLine"
+ // Change this line // Change this line
+
+ console.log(myStr);
