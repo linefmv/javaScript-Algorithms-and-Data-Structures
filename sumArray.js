9c5a47d05db2a7b1758c78e93f4a342566924f21
Aline Viana <alinemaciel018@gmail.com>
2021-08-31 13:15:40 -0300

    Exercises

---
+++ sumArray.js
@@ -0,0 +1,12 @@
+// Declare e inicialize uma variável totalpara 0. Use um forloop para adicionar o valor de cada elemento da myArrmatriz total.
+
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0
+for (var i = 0; i < myArr.length; i++) {
+   console.log(myArr[i]);
+   total += myArr[i]
+}
