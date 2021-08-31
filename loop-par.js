95711af63dd2581701f8be0a0edba6b9574db0ba
Aline Viana <alinemaciel018@gmail.com>
2021-08-31 12:49:13 -0300

    Exercises

---
+++ loop-par.js
@@ -0,0 +1,20 @@
+// Repita os números ímpares com um loop for
+// Os loops For não precisam iterar um de cada vez. Ao mudar o nosso final-expression, podemos contar por números pares.
+
+// Vamos começar em i = 0e fazer um loop while i < 10. Iremos incrementar iem 2 cada loop com i += 2.
+
+// var ourArray = [];
+// for (var i = 0; i < 10; i += 2) {
+//   ourArray.push(i);
+// }
+// ourArrayagora conterá [0,2,4,6,8]. Vamos mudar o nosso initializationpara que possamos contar por números ímpares.
+
+// Empurre os números ímpares de 1 a 9 para myArrayusar um forloop.
+
+var myArray = [];
+
+// Only change code below this line
+var myArray = [];
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
