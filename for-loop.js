52b5846fb620a69f474fefeaddeee44a17feaa81
Aline Viana <alinemaciel018@gmail.com>
2021-08-31 12:43:10 -0300

    Exercises

---
+++ for-loop.js
@@ -0,0 +1,9 @@
+// Use um forloop para enviar os valores de 1 a 5 myArray.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1; i <= 5; i++) {
+  myArray.push(i);
+   }
